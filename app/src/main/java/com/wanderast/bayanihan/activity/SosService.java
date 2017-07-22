8e5e809f8c688e9859f801a63465f9bbf886bc9e
Archie <archie.mateo@descouvre.com>
2017-07-22 21:23:37 +0800

    working gps

--- app/src/main/java/com/wanderast/bayanihan/activity/SosService.java
+++ app/src/main/java/com/wanderast/bayanihan/activity/SosService.java
@@ -14,8 +14,13 @@ import android.util.Log;
 
 public class SosService extends IntentService {
 
-    LocationManager locationManager;
     Double longitude, latitude;
+
+    GpsLocator locationListener;
+
+    LocationManager locationManager;
+
+    Location location = null;
 
     public SosService() {
         super("SosService");
@@ -26,47 +31,46 @@ public class SosService extends IntentService {
 
         Log.v("Service", "Running");
 
-        GpsLocator gps = new GpsLocator();
-
+        locationListener = new GpsLocator();
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
+        while(location == null) {
+            getLocation();
+        }
+
+        Log.d("END:", "fin");
+
+    }
+
+    public void setLocation(Location location) {
+        if(location != null && location.getAccuracy() < 20) {
+            longitude = location.getLongitude();
+            latitude = location.getLatitude();
+            return;
+        }
+        getLocation();
+    }
+
+     public class GpsLocator implements LocationListener {
+        public void onLocationChanged(Location location) {
+            // Called when a new location is found by the network location provider.
+            setLocation(location);
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) { Log.d("asdasdss", "asdasd"); }
+
+        public void onProviderEnabled(String provider) { Log.d("asdasdss", "asdasd"); }
+
+        public void onProviderDisabled(String provider) { Log.d("asdasdss", "asdasd"); }
+    }
+
+    public void getLocation() {
+
         try {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, gps);
-            locationManager.removeUpdates(gps);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, locationListener);
+            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
         } catch (SecurityException e) {
             e.printStackTrace();
         }
-
     }
-
-    public class GpsLocator implements LocationListener {
-
-        @Override
-        public void onLocationChanged(Location location) {
-            setLocation(location.getLongitude(), location.getLatitude());
-            Log.v("Longitude", longitude.toString());
-            Log.v("Latitude", latitude.toString());
-        }
-
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-
-        }
-
-        @Override
-        public void onProviderEnabled(String provider) {
-
-        }
-
-        @Override
-        public void onProviderDisabled(String provider) {
-
-        }
-    }
-
-    public void setLocation(Double lon, Double lat) {
-        longitude = lon;
-        latitude = lat;
-    }
-
 }
